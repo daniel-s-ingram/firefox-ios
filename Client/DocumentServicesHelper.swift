0bb2f27faa34a76b54dfe1cef171782567646520
farhanpatel <farhanpatel.17@gmail.com>
2018-12-10 13:58:50 -0500

    Bug 1512172 - Use NSLinguisticTagger for detecting the dominant language for translation

--- Client/DocumentServicesHelper.swift
+++ Client/DocumentServicesHelper.swift
@@ -6,6 +6,7 @@ import JavaScriptCore
 import Shared
 import Storage
 import XCGLogger
+import NaturalLanguage
 
 private let log = Logger.browserLogger
 
@@ -20,39 +21,6 @@ class DocumentServicesHelper: TabEventHandler {
         return queue
     }()
 
-    private lazy var context: JSContext = {
-        let virtualMachine = JSVirtualMachine()
-        let context: JSContext = JSContext(virtualMachine: virtualMachine)
-
-        context.exceptionHandler = { context, exception in
-            if let exception = exception {
-                log.error("DocumentServices.js: \(exception)")
-            }
-        }
-
-        let name = "DocumentServices"
-        guard let path = Bundle.main.path(forResource: name, ofType: "js"),
-            let jsFile = try? String(contentsOfFile: path, encoding: .utf8) else {
-            log.error("DocumentServices are unavailable due to missing or corrupt JS file")
-            return context
-        }
-
-        context.evaluateScript("var __firefox__;")
-        context.evaluateScript(jsFile)
-
-        return context
-    }()
-
-    private lazy var documentServices: JSValue? = {
-        guard let firefox = context.objectForKeyedSubscript("__firefox__"),
-            let isConfigured = firefox.objectForKeyedSubscript("isConfigured"),
-            isConfigured.isBoolean && isConfigured.toBool() else {
-                log.error("Unable to do anything without a firefox object.")
-                return nil
-        }
-        return firefox
-    }()
-
     init() {
         self.tabObservers = registerFor(
             .didLoadPageMetadata,
@@ -64,42 +32,19 @@ class DocumentServicesHelper: TabEventHandler {
     }
 
     func tab(_ tab: Tab, didLoadPageMetadata metadata: PageMetadata) {
-        let analyze = documentServices?.objectForKeyedSubscript("analyze")
-        guard let jsValue = analyze?.call(withArguments: [metadata.toDictionary() as NSDictionary]), jsValue.isObject else {
-            log.error("There was some problem in DocumentServices.js, see the log above")
-            return
+        guard let text = metadata.description else { return }
+        let language: String?
+        if #available(iOS 12.0, *) {
+            language = NLLanguageRecognizer.dominantLanguage(for: text)?.rawValue
+        } else {
+            language = NSLinguisticTagger.dominantLanguage(for: text)
         }
-
-        guard let dict = jsValue.toDictionary() as? [String: Any],
-            !dict.isEmpty else {
-            log.debug("Nothing interesting came back from DocumentServices. Exiting.")
-            return
-        }
-
-        let derived = DerivedMetadata.fromDictionary(dict)
-
-        // XXX we came from the main thread (.PageMedataParser) and we're posting
-        // to the main thread for most of our use cases.
-        // NotificationCenter is deadlocked when we do that directly, so
-        // post to NotificationCenter indirectly.
         DispatchQueue.global().async {
-            TabEvent.post(.didDeriveMetadata(derived), for: tab)
+            TabEvent.post(.didDeriveMetadata(DerivedMetadata(language: language)), for: tab)
         }
     }
 }
 
 struct DerivedMetadata {
     let language: String?
-
-    static func fromDictionary(_ d: [String: Any]) -> DerivedMetadata {
-        let language: String?
-        if let lang = d["language"] as? [String: String],
-            let identifier = lang["iso639_1"] ?? lang["iso639_2T"] {
-            language = Locale.canonicalLanguageIdentifier(from: identifier)
-        } else {
-            language = nil
-        }
-
-        return DerivedMetadata(language: language)
-    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,35 @@
 const glob = require("glob");
 const path = require("path");
-
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const AllFramesAtDocumentStart = glob.sync("./Client/Frontend/UserContent/UserScripts/AllFrames/AtDocumentStart/*.js");
 const AllFramesAtDocumentEnd = glob.sync("./Client/Frontend/UserContent/UserScripts/AllFrames/AtDocumentEnd/*.js");
 const MainFrameAtDocumentStart = glob.sync("./Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentStart/*.js");
 const MainFrameAtDocumentEnd = glob.sync("./Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentEnd/*.js");
-const DocumentServices = glob.sync("./Client/Frontend/UserContent/UserScripts/DocumentServices/src/*.js");
+
+MainFrameAtDocumentStart.push("./content-blocker-lib-ios/js/TrackingProtectionStats.js");
 
 // Ensure the first script loaded at document start is __firefox__.js
 // since it defines the `window.__firefox__` global.
-const needsFirefoxFile = {
-  AllFramesAtDocumentStart,
+if (path.basename(AllFramesAtDocumentStart[0]) !== "__firefox__.js") {
+  throw "ERROR: __firefox__.js is expected to be the first script in AllFramesAtDocumentStart.js";
+}
 
-  // PDF content does not execute user scripts designated to
-  // run at document start for some reason. So, we also need
-  // to include __firefox__.js for the document end scripts.
-  // ¯\_(ツ)_/¯
-  AllFramesAtDocumentEnd,
-
-  DocumentServices
-};
-
-for (let [name, files] of Object.entries(needsFirefoxFile)) {
-  if (path.basename(files[0]) !== "__firefox__.js") {
-    throw `ERROR: __firefox__.js is expected to be the first script in ${name}.js`;
-  }
+// Ensure the first script loaded at document end is __firefox__.js
+// since it also defines the `window.__firefox__` global because PDF
+// content does not execute user scripts designated to run at document
+// start for some reason. ¯\_(ツ)_/¯
+if (path.basename(AllFramesAtDocumentEnd[0]) !== "__firefox__.js") {
+  throw "ERROR: __firefox__.js is expected to be the first script in AllFramesAtDocumentEnd.js";
 }
 
 module.exports = {
   mode: "production",
   entry: {
-    AllFramesAtDocumentStart,
-    AllFramesAtDocumentEnd,
-    MainFrameAtDocumentStart,
-    MainFrameAtDocumentEnd,
-    DocumentServices,
+    AllFramesAtDocumentStart: AllFramesAtDocumentStart,
+    AllFramesAtDocumentEnd: AllFramesAtDocumentEnd,
+    MainFrameAtDocumentStart: MainFrameAtDocumentStart,
+    MainFrameAtDocumentEnd: MainFrameAtDocumentEnd
   },
   output: {
     filename: "[name].js",
